c441be51e2bcf537d944ca5a68bd0c16230feb70
nkathawa <nkathawa@umich.edu>
2020-05-01 23:48:09 -0400

    module updates and json with only memes we want

--- util/get_memes.py
+++ util/get_memes.py
@@ -1,12 +1,22 @@
 import requests
+import json
 
 resp = requests.get('https://api.imgflip.com/get_memes')
 if resp.status_code != 200:
     # This means something went wrong.
     raise ApiError('GET /get_memes/ {}'.format(resp.status_code))
 
-for todo_item in resp.json()["data"]["memes"]:
-	if todo_item['box_count'] == 2:
-		print('{},{}'.format(todo_item['name'], todo_item['url']))
-		#myfile = requests.get(todo_item['url'])
-		#open(todo_item['name'].replace(" ", "")+'.png', 'wb').write(myfile.content)
+# memes = resp.json()["data"]["memes"]
+# print memes
+
+my_data = {}
+for meme in resp.json()["data"]["memes"]:
+	if meme['box_count'] == 2:
+		data = {'name': meme['name'], 'url': meme['url']}
+		my_data[meme['id']] = data
+		# myfile = requests.get(meme['url'])
+		# open(meme['name'].replace(" ", "").replace("?", "") +'.png', 'wb').write(myfile.content)
+json_data = json.dumps(my_data)
+sample = open('samplefile.txt', 'w') 
+sample.write(json_data) 
+sample.close()
