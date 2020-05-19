2fbe5d7f502a7bc7e0536723f59fcabfee771b1f
nkathawa <nkathawa@umich.edu>
2020-05-18 21:13:17 -0400

    flesh out details of auth

--- platform/authentication.py
+++ platform/authentication.py
@@ -1,34 +1,41 @@
 from random import randint
 
 class Player():
-	def __init__(self, uuid, name):
-		self.uuid = ""
-		self.name = ""
+	def __init__(self, name):
+		self.uuid = randint(100000000, 999999999)
+		self.name = name
 
 class Authentication():
 	def __init__(self, games):
-		self.games = {}
+		self.games = games
 
 	def generate_random_code(self):
 		return randint(100000000, 999999999)
 
-	def start_game(self, username):
+	def start_game(self, player):
 		code = self.generate_random_code()
-		self.games[code] = [username]
+		self.games[code] = [player]
 		return code
 
-	def join_game(self, code, username):
-		self.games[code].append(username)
+	def join_game(self, code, player):
+		self.games[code].append(player)
 
 def main():
+	navin = Player("navin")
+	john = Player("john")
+	javier = Player("javier")
+	anthony = Player("anthony")
+	josiah = Player("josiah")
+	andrew = Player("andrew")
+
 	games = {}
 	authentication = Authentication(games)
-	code = authentication.start_game("navin")
-	authentication.join_game(code, "john")
-	code = authentication.start_game("javier")
-	authentication.join_game(code, "anthony")
-	code = authentication.start_game("josiah")
-	authentication.join_game(code, "andrew")
+	code = authentication.start_game(navin)
+	authentication.join_game(code, john)
+	code = authentication.start_game(javier)
+	authentication.join_game(code, anthony)
+	code = authentication.start_game(josiah)
+	authentication.join_game(code, andrew)
 	print(authentication.games)
 
 if __name__ == "__main__":
